ff9cc5d69647ed5cc20fef424e78de261707ba71
Elias Silva <eas-lias@hotmail.com>
2017-12-24 13:40:56 -0200

    Inicial
    carregando o projeto

---
+++ TratandoString/Conversoes.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using Newtonsoft.Json;
+
+
+namespace TratandoString
+{
+
+    //public class Teste
+    //{
+    //    public int Idteste { get; set; }
+    //    public string NomeTeste { get; set; }
+    //    public double ValorTeste { get; set; }
+    //}
+
+
+    public class Conversoes
+    {
+
+        ///// <summary>
+        ///// Apenas para teste
+        ///// </summary>
+        //static void Main()
+        //{
+        //    var teste = new Teste();
+
+        //    teste.Idteste = 0;
+        //    teste.NomeTeste = "Teste";
+        //    teste.ValorTeste = 100.0;
+
+        //    Conversoes c = new Conversoes();
+
+        //    var testeXml = c.ObjectToXml<Teste>(teste);
+        //    var testeJson = c.ObjectToJson<Teste>(teste);
+
+        //    teste = c.XmlToObject<Teste>(testeXml);
+        //    teste = c.JsonToObject<Teste>(testeJson);
+        //}
+
+
+        /// <summary>
+        /// Este método recebe um Objecto e retorna o mesmo objeto em Formato Json
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="objeto"></param>
+        /// <returns></returns>
+        public string ObjectToXml<T>(T objeto)
+        {
+            MemoryStream oStream = new MemoryStream();
+            XmlSerializer serializador = new XmlSerializer(typeof(T));
+            serializador.Serialize(oStream, objeto);
+            string retorno = Encoding.UTF8.GetString(oStream.ToArray());
+            return retorno;
+        }
+
+
+        /// <summary>
+        /// Este método recebe um Xml e transforma o mesmo em um Object correspondente
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="Xml"></param>
+        /// <returns></returns>
+        public T XmlToObject<T>(string Xml)
+        {
+
+            MemoryStream oStream = new MemoryStream(Encoding.UTF8.GetBytes(Xml));
+            XmlSerializer deserializador = new XmlSerializer(typeof(T));
+            T retorno = (T)deserializador.Deserialize(oStream);
+            return retorno;
+        }
+
+        /// <summary>
+        /// Este método recebe um Objeto e retorna o mesmo objeto em formato Json
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="objeto"></param>
+        /// <returns></returns>
+        public string ObjectToJson<T>(T objeto)
+        {
+            MemoryStream oStream = new MemoryStream();
+            DataContractJsonSerializer serializador = new DataContractJsonSerializer(typeof(T));
+            serializador.WriteObject(oStream, objeto);
+            string retorno = Encoding.UTF8.GetString(oStream.ToArray());
+            return retorno;
+        }
+
+
+        /// <summary>
+        /// Este método recebe um string Json e retorna o objeto deste Json
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        public T JsonToObject<T> (string json)
+        {
+
+            MemoryStream oStream = new MemoryStream(Encoding.UTF8.GetBytes(json));
+            DataContractJsonSerializer deserializador = new DataContractJsonSerializer(typeof(T));
+            T retorno = (T)deserializador.ReadObject(oStream);
+            return retorno;
+
+        }
+
+    }
+}
+
+
+
+
---
+++ TratandoString/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("TratandoString")]
+[assembly: AssemblyDescription("Projeto Pessoal")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Elias Silva")]
+[assembly: AssemblyProduct("TratandoString")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("Elias Silva")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("274e51a3-eb2d-49ff-b928-37dbf06872fa")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o '*' como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
